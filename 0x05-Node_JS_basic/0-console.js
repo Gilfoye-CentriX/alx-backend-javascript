75ba61184f6d9f2a3b28aa1e4cd38fd64c4ce2a7
Gilfoye-CentriX <125440612+Gilfoye-CentriX@users.noreply.github.com>
2024-04-30 16:35:24 +0200

    Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Mani Dlamini <https://github.com/Gilfoye-Centrix>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
